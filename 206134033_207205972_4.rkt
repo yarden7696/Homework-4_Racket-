e0e1cd1f71d42581bfe91a6add26bd732d3c4c7d
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2021-06-09 19:06:51 +0300

    Add files via upload

---
+++ 206134033_207205972_4.rkt
@@ -0,0 +1,360 @@
+#lang pl
+
+#| role : we do all the assianment together |#
+
+;;-----------------------------------------------Part A------------------------------------------------------
+ 
+;; ---------------------------------------------Question 1---------------------------------------------------
+
+#| This question was not difficult for us and took us an average of 2 minutes
+
+
+<SOL> :: = { <NumList> }
+        |  { scalar-mult <num> <SOL> }
+        |  { intersect <SOL> <SOL>} ;;intersect between 2 SOLS
+        |  { union <SOL> <SOL> } ;; union between 2 SOLS
+        |  <id>
+        |  { with {<id> <SOL>  <id> <SOL>} <SOL> } ;; this should be a syntactic sugar
+        |  { fun { <id> <id> } <SOL> } ;; a function must have exactly two formal parameters
+        |  { call-static <SOL> <SOL> <SOL> } ;; extends closure environment
+        |  { call-dynamic <SOL> <SOL> <SOL> } ;; extends current environment
+
+        |  True
+        |  False ;; false is SOL too caz its in the valid expression 
+        | { if <SOL> then <SOL> else <SOL> }
+        | { equal? <SOL> <SOL> }
+
+<NumList> :: =  λ | <num> <NumList> ;; where λ stands for the empty word, i.e., { } is the empty set
+
+;; where <num> is any expression identified by Racket as a Number
+;; and <id> is any expression such that Racket identifies '<id> as a symbol
+ 
+|#
+
+
+;; -----------------------------------------------------
+;;This question was not difficult for us and took us about 2 minutes
+
+;; The abstract syntax tree SOL
+(define-type SET = (Listof Number))
+
+(define-type SOL
+  ;; Please complete the missing parts -- you are NOT allowed to use additional variants (constructors)
+  [Set  SET] ;; Set is a list of numbers so he got Set
+  [Smult Number SOL] ;; Smul- miltiply by scalar so he got number and SOL
+  [Inter SOL SOL] ;;Inter is a binary operator so he needs to get 2 SOL
+  [Union SOL SOL] ;;Union is a binary operator so he needs to get 2 SOL
+  [Id    Symbol]
+  ;;    [With  Symbol Symbol SOL SOL SOL] -- not to be used, syntactic sugar for ...
+  [Fun   Symbol Symbol SOL] ;; Fun gets 2 symbols and body
+  [CallS SOL SOL SOL] ;; (CallS(Fun (name1 name2) parse-expr(body)) parse-expr(named1) parse-expr(named2))  
+  [CallD SOL SOL SOL] ;; (CallD(Fun (name1 name2) parse-expr(body)) parse-expr(named1) parse-expr(named2))
+  [Bool Boolean] ;; True/False
+  [If SOL SOL SOL] ;; (If (condition-SOL1) (do SOL2-if the condition exist) (do SOL3-if the condition not exist) 
+  [Equal SOL SOL])
+
+
+;; ----------------------------------------------------
+;; Operations on SETs
+;; Please complete the missing parts, and add comments (comments should specify 
+;; the role of each procedure, but also describe your work process). Keep your code readable. 
+
+(: ismember? : Number SET  -> Boolean)
+(define (ismember? n l)
+  (cond [(null? l) #f]
+        [(= n (first l)) #t]
+        [else (ismember? n (rest l))]))
+
+(test (not (ismember? 1 '(3 4 5))))
+(test (not (ismember? 1 '( 3 2 3 5 6))))
+(test (ismember? 1 '(3 4 5 1 3 4)))
+(test (ismember? 1 '(1)))
+
+(: remove-duplicates : SET  -> SET)
+(define (remove-duplicates l)
+  (cond [(or (null? l) (null? (rest l))) l]
+        [(ismember? (first l) (rest l)) (remove-duplicates (rest l))]
+        [else (cons (first l) (remove-duplicates (rest l)))]))
+  
+(: create-sorted-set : SET -> SET)
+(define (create-sorted-set l)
+  (remove-duplicates (sort l <)))
+  
+(: set-union : SET SET -> SET)
+(define (set-union A B)
+  (create-sorted-set (append A B)))
+
+(: set-intersection : SET SET -> SET)
+(define (set-intersection A B)
+  (: mem-filter : Number -> Boolean)
+  (define (mem-filter n)
+    (ismember? n A))
+  (filter mem-filter (create-sorted-set B)))
+
+  
+
+
+;; -----------------------------------Question 2------------------------------------------
+;; Parser
+
+(: parse-sexpr : Sexpr -> SOL)
+;; to convert s-expressions into SOLs
+(define (parse-sexpr sexpr)
+  (match sexpr
+    [(list (number: ns) ...) (Set (create-sorted-set ns))] ;; sort and remove-duplicates
+    ['True (Bool true)] ;; true constructor
+    ['False (Bool false)] ;; false constructor
+    [(symbol: name) (Id name)]
+    [(cons 'with more) ;; if we have a pair that his first element is the word 'with and the second elem is something
+       (match sexpr ;; we need to check this sexpr
+         [(list 'with (list (symbol: name1) named1 (symbol: name2) named2) body)
+          (CallS (Fun name1 name2 (parse-sexpr body)) (parse-sexpr named1) (parse-sexpr named2))] ;;; There is no With constructor. Replace it with existing constructors...
+         [else (error 'parse-sexpr "bad `with' syntax in ~s" sexpr)])] ;;invalid sexpr 
+    [(cons 'fun more);; if we have a pair that his first element is the word 'fun and the second elem is something
+       (match sexpr ;; we need to check this sexpr
+         [(list 'fun (list (symbol: name1) (symbol: name2)) body) ;; valid sexpr 
+          (if (equal? name1 name2)
+              (error 'parse-sexpr "`fun' has a duplicate param name in ~s" sexpr) ;; cannot use the same param name twice
+              (Fun name1 name2 (parse-sexpr body)))] ;; if name1 and name2 not equal
+         [else (error 'parse-sexpr "bad `fun' syntax in ~s" sexpr)])] ;; invalid sexpr
+    
+    [(list 'scalar-mult (number: sc) rhs) (Smult sc (parse-sexpr rhs))];;calling 'Smult' cons with sc and send rhs to 'parse-sexprS'  
+    [(list 'intersect lhs rhs) (Inter (parse-sexpr lhs) (parse-sexpr rhs))];; calling 'Inter' const and send lhs,rhs to 'parse-sexprS' 
+    [(list 'union lhs rhs) (Union (parse-sexpr lhs) (parse-sexpr rhs))];; calling 'Union' const and send lhs,rhs to 'parse-sexprS'
+    [(list 'call-static fun arg1 arg2) (CallS (parse-sexpr fun) (parse-sexpr arg1) (parse-sexpr arg2))]
+    [(list 'call-dynamic fun arg1 arg2) (CallD (parse-sexpr fun) (parse-sexpr arg1) (parse-sexpr arg2))]
+    [(list 'if cond 'then true-cond 'else false-cond) (If (parse-sexpr cond) (parse-sexpr true-cond) (parse-sexpr false-cond))] 
+    [(list 'equal? sol1 sol2) (Equal (parse-sexpr sol1) (parse-sexpr sol2))] 
+    [else (error 'parse-sexpr "bad syntax in ~s" sexpr)]))
+
+
+(: parse : String -> SOL)
+;; parses a string containing a SOL expression to a SOL AST
+(define (parse str)
+  (parse-sexpr (string->sexpr str)))
+
+  
+
+;;; Tests for parse
+ 
+(test (parse "{1 2 3  4 1 4  4 2 3 4 1 2 3}") => (Set '(1 2 3 4)))
+(test (parse "{union {1 2 3} {4 2 3}}") => (Union (Set '(1 2 3)) (Set '(2 3 4))))
+(test (parse "{fun {x x} x}") =error> "parse-sexpr: `fun' has a duplicate param name in (fun (x x) x)")
+(test (parse "{intersect {1 2 3} {4 2 3}}") => (Inter (Set '(1 2 3)) (Set '(2 3 4))))
+(test (parse "{with {S {intersect {1 2 3} {4 2 3}} c {}}
+                 {call-static {fun {x y} {union x S}}
+                              {scalar-mult 3 S}
+                              {4 5 7 6 9 8 8 8}}}") 
+      =>
+      (CallS (Fun 'S
+                  'c
+                  (CallS (Fun 'x 'y (Union (Id 'x) (Id 'S))) 
+                         (Smult 3 (Id 'S)) 
+                         (Set '(4 5 6 7 8 9))))
+             (Inter (Set '(1 2 3)) (Set '(2 3 4)))
+             (Set '())))
+
+
+(test (parse "{with {S {intersect {1 2 3} {4 2 3}} S1 {union {1 2 3} {4 2 3}}}
+                          {fun {x} S}}")
+      =error> "parse-sexpr: bad `fun' syntax in (fun (x) S)") ;; functions require two formal parameters
+(test (parse "True") => (Bool true))
+(test (parse "{if {equal? {1 2 3} {1 2}} then {1 2 3} else {1 2}}") =>
+      (If (Equal (Set '(1 2 3)) (Set '(1 2))) (Set '(1 2 3)) (Set '(1 2))))
+
+(test (parse "{with {S {intersect {1 2 3} {4 2 3}} c {}}
+                 {call-dynamic {fun {x y} {union x S}}
+                               {if {equal? S {scalar-mult 3 S}}
+                                   then S
+                                   else {4 5 7 6 9 8 8 8}}
+                               {}}}")
+=> (CallS (Fun 'S 'c
+               (CallD (Fun 'x 'y (Union (Id 'x) (Id 'S)))
+                      (If (Equal (Id 'S) (Smult 3 (Id 'S)))
+                          (Id 'S)
+                          (Set '(4 5 6 7 8 9)))
+                      (Set '())))
+          (Inter (Set '(1 2 3)) (Set '(2 3 4)))
+          (Set '())))
+
+;;-----------------------------------------------------
+;; Evaluation 
+#|
+------------------------------------------------------
+Evaluation rules:
+    ;; Please complete the missing parts in the formal specifications below
+    eval({ N1 N2 ... Nl })      = sort( create-set({ N1 N2 ... Nl })) ;; where create-set removes all duplications from
+                                                                         the sequence (list) and sort is a sorting procedure
+
+    eval({scalar-mult K E})     = { K*N1 K*N2 ... K*Nl }              ;; where eval(E)={ N1 N2 ... Nl }
+    eval({intersect E1 E2})     = sort( create-set(set-intersection (eval(E1) , eval(E2)))     
+    eval({union E1 E2})         = sort( create-set(set-union (eval(E1) , eval(E2)))
+    eval({fun {x1 x2} E},env)   = <{fun {x1 x2} E}, env>
+    eval({call-static E-op E1 E2},env)
+                                = eval(Ef,extend(x2,eval(E2,env) ... <-- fill in --> )
+                                                      if eval(E-op,env) = <{fun {x1 x2} Ef}, envf>
+                                = error!              otherwise
+    eval({call-dynamic E-op E1 E2},env)
+                                = <-- fill in -->
+                                                      if <-- fill in -->
+                                = error!              otherwise
+
+    eval(True,env)              = true
+    eval(False,env)             = <-- fill in -->
+    eval({if E1 then E2 else E3},env)
+                                = eval(E3, env)       if eval(E1,env) = false
+                                = <-- fill in -->     otherwise
+
+    eval({equal? E1 E2},env)    = true                if eval(E1,env) is equal in content to eval(E2,env)
+                                = false               otherwise
+
+
+
+
+
+|#
+
+;; Types for environments, values, and a lookup function
+
+(define-type ENV
+  [EmptyEnv]
+  [Extend Symbol VAL ENV])
+
+(define-type VAL
+  [SetV SET]
+  [FunV Symbol Symbol SOL ENV]
+  [BoolV <-- fill in -->]) 
+
+(: lookup : Symbol ENV -> VAL)
+(define (lookup name env)
+  (cases env
+    [(EmptyEnv) (error 'lookup "no binding for ~s" name)]
+    [(Extend id val rest-env)
+     (if (eq? id name) val (lookup name rest-env))]))
+
+
+;; Auxiliary procedures for eval 
+;; Please complete the missing parts, and add comments (comments should specify 
+;; the role of each procedure, but also describe your work process). Keep your code readable. 
+
+(: SetV->set : VAL -> SET)
+(define (SetV->set v)
+  (cases v
+    [(SetV S) S]
+    [else (error 'SetV->set "expects a set, got: ~s" v)]))
+  
+(: smult-set : Number VAL -> VAL)
+(define (smult-set n s)
+  (: mult-op : Number -> Number)
+  (define (mult-op k)
+    (* k n))
+  (<-- fill in --> (map <-- fill in -->)))
+
+
+
+(: set-op : <-- fill in --> )
+;; gets a binary SET operator, and uses it within a SetV
+;; wrapper
+(define (set-op op val1 val2)
+  (SetV (op (SetV->set val1) (SetV->set val2))))
+
+;;---------  the eval procedure ------------------------------
+;; Please complete the missing parts, and add comments (comments should specify 
+;; the choices you make, and also describe your work process). Keep your code readable. 
+(: eval : SOL ENV -> VAL)
+;; evaluates SOL expressions by reducing them to set values
+(define (eval expr env)
+  (cases expr
+    [(Set S) <-- fill in -->]
+    [(Smult n set) (smult-set <-- fill in -->)]
+    [(Inter l r) (set-op set-intersection <-- fill in -->)]
+    [(Union l r) <-- fill in -->]
+    [(Id name) (lookup name env)]
+    [(Fun bound-id1 bound-id2 bound-body)
+     (FunV bound-id1 bound-id2 bound-body env)]
+    [(CallS fun-expr arg-expr1 arg-expr2)
+     (let ([fval (eval fun-expr env)])
+       (cases fval
+         [(FunV bound-id1 bound-id2 bound-body f-env)
+          <-- fill in -->]
+         [else (error 'eval "`call-static' expects a function, got: ~s"
+                      fval)]))]
+    [(CallD fun-expr arg-expr1 arg-expr2)
+     (let ([fval (eval fun-expr env)])
+       (cases fval
+         [<-- fill in -->]
+         [else (error 'eval "`call-dynamic' expects a function, got: ~s"
+                      fval)]))]
+    [(Bool b) <-- fill in -->]
+    [(If cond true-cond false-cond)
+     (let ([cval <-- fill in -->])
+       (cases cval
+         [(BoolV b) <-- fill in -->] ;; b is a Boolean value 
+         [else <-- fill in -->]))] 
+    [(Equal l r) (if (equal? (eval l env) (eval r env)) <-- fill in -->)]))
+
+ 
+
+
+(: createGlobalEnv : -> ENV)
+(define (createGlobalEnv)
+  (Extend 'second <-- fill in -->
+          (Extend <-- fill in -->
+                  (Extend <-- fill in --> 
+                          (EmptyEnv)))))
+
+(: run : String -> (U SET VAL Boolean))
+;; evaluate a SOL program contained in a string
+(define (run str)
+  (let ([result (eval (parse str) <-- fill in -->)])
+    (cases result
+      [(SetV S) <-- fill in -->]
+      [<-- fill in -->]
+      [else <-- fill in -->])))
+
+
+(test (run "{1 2 3  4 1 4  4 2 3 4 1 2 3}") => '(1 2 3 4))
+(test (run "{union {1 2 3} {4 2 3}}") => '(1 2 3 4))
+(test (run "{intersect {1 2 3} {4 2 3}}") => '( 2 3))
+(test (run "{with {S {intersect {1 2 3} {4 2 3}}
+                   S1 {}}
+                 {call-static {fun {x y} {union x S}}
+                              {scalar-mult 3 S}
+                              {4 5 7 6 9 8 8 8}}}")
+      => '(2 3 6 9))
+
+
+(test (run "{with {S {intersect {1 2 3} {4 2 3}}
+                   S1 {}}
+               {call-static {fun {x y} {union x y}}
+                              {scalar-mult 3 S}
+                              {4 5 7 6 9 8 8 8}}}")
+      => '(4 5 6 7 8 9))
+
+(test (run "{with {p {call-static cons {1 2 3} {4 2 3}}
+                    S1 {}}
+              {with {S {intersect {call-static first p {}}
+                                  {call-static second p {}}}
+                     S1 {}}
+                 {call-static {fun {x y} {union x S}}
+                              {scalar-mult 3 S}
+                              {4 5 7 6 9 8 8 8}}}}")
+      =>  '(2 3 6 9))
+(test (run "{fun {x x} x}") =error> "parse-sexpr: `fun' has a duplicate param name in (fun (x x) x)")
+
+(test (run "{with {p {call-dynamic cons {1 2 3} {4 2 3}}
+                   S1 {}}
+              {with {S {intersect {call-dynamic first p {}}
+                                  {call-dynamic second p {}}}
+                     S1 {}}
+                 {call-dynamic {fun {x y} {union x S}}
+                              {scalar-mult 3 S}
+                              {4 5 7 6 9 8 8 8}}}}")
+      =>  '(2 3 6 9))
+(test (run "{call-static {1} {2 2} {}}")
+      =error> "eval: `call-static' expects a function, got: #(struct:SetV (1))")
+(test (run "True") => #t)
+(test (run "{if {equal? {1 2 3} {1 2}} then {1 2 3} else {1 2}}") => '(1 2))
+(test (run "{equal? {union {1 2 3} {4 2 3}} {1 2 3 4}}") => #t)
+(test (run "{union {equal? {4} {4}} {4 2 3}}") =error> "SetV->set: expects a set, got: #(struct:BoolV #t)")
+
